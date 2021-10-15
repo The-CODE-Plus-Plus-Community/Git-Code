d9593210040f4511e7f2c563bd8b9f6d89bec5db
Soumyakanta Mohanty <soumyakantam022@gmail.com>
2021-10-15 22:56:41 +0530

    minor changes

--- components/index/Stats.js
+++ components/index/Stats.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 function Stats() {
     return (
-        <div class="flex-container">
-        <div class="flex-item bg-one tilt">
+        <div className="flex-container">
+        <div className="flex-item bg-one tilt">
             <a href="#" > <span className="text">70k+ Contributors</span> </a>
         </div>
-        <div class="flex-item bg-two tilt">
+        <div className="flex-item bg-two tilt">
             <a href="#" > <span className="text">50+ Hours of Tutorial Content</span> </a>
         </div>
-        <div class="flex-item bg-three tilt">
+        <div className="flex-item bg-three tilt">
             <a href="#" target="_blank"> <span className="text">100+ Ongoing Projects</span> </a>
         </div>
     </div>
